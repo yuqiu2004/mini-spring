712c107140b01ca9f7e7dcb6c3b03f8802f549eb
yuqiu2004 <2506852507@qq.com>
2024-10-14 12:19:08 +0800

    feat : 完善属性和方法

--- src/main/java/org/example/beans/config/BeanDefinition.java
+++ src/main/java/org/example/beans/config/BeanDefinition.java
@@ -1,23 +1,71 @@
 package org.example.beans.config;
 
-import org.example.beans.config.PropertyValues;
+import java.util.Objects;
 
 /**
- * 保存bean信息
+ * 保存bean信息 class类型+bean属性
  */
 public class BeanDefinition {
+
+    public static String SCOPE_SINGLETON = "singleton";
+
+    public static String SCOPE_PROTOTYPE = "prototype";
+
+    /**
+     * bean class类
+     */
     private Class beanClass;
 
+    /**
+     * class 属性
+     */
     private PropertyValues propertyValues;
 
-    public BeanDefinition(){}
+    /**
+     * 初始化方法名称
+     */
+    private String initMethodName;
+
+    /**
+     * 销毁方法名称
+     */
+    private String destroyMethodName;
+
+    /**
+     * 作用域 默认单例
+     */
+    private String scope = SCOPE_SINGLETON;
+
+    private boolean singleton = true;
+
+    private boolean prototype = false;
+
+    /**
+     * 懒加载
+     */
+    private boolean lazyInit = false;
+
     public BeanDefinition(Class<?> clazz){
         this.beanClass = clazz;
     }
 
     public BeanDefinition(Class beanClass, PropertyValues propertyValues) {
         this.beanClass = beanClass;
-        this.propertyValues = propertyValues;
+        this.propertyValues = propertyValues!=null ? propertyValues : new PropertyValues();
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+        this.singleton = SCOPE_SINGLETON.equals(scope);
+        this.prototype = SCOPE_PROTOTYPE.equals(scope);
+    }
+
+    public boolean isSingleton() {
+        return this.singleton;
+    }
+
+    public boolean isPrototype() {
+        return this.prototype;
     }
 
     public Class getBeanClass() {
@@ -34,5 +82,42 @@ public class BeanDefinition {
 
     public void setPropertyValues(PropertyValues propertyValues) {
         this.propertyValues = propertyValues;
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    public String getDestroyMethodName() {
+        return destroyMethodName;
+    }
+
+    public void setDestroyMethodName(String destroyMethodName) {
+        this.destroyMethodName = destroyMethodName;
+    }
+
+    public void setLazyInit(boolean lazyInit) {
+        this.lazyInit = lazyInit;
+    }
+
+    public boolean isLazyInit() {
+        return lazyInit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BeanDefinition that = (BeanDefinition) o;
+        return beanClass.equals(that.beanClass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(beanClass);
     }
 }
